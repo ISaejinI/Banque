90fe4f419f130bea0c8614a52c88c723b17505ab
Lou-Anne <saejinmx@gmail.com>
2025-10-20 19:39:35 +0200

    Création de nouveau client fonctionnel

--- index.js
+++ index.js
@@ -16,3 +16,17 @@ let datas = {
 
 const generateRandomId = () => crypto.randomUUID();
 
+function createClient(name, lastName) {
+    if (typeof name === "string" && typeof lastName === "string") {
+        const newClient = {
+            id: generateRandomId(),
+            name,
+            lastName
+        }
+        datas.clients.push(newClient);
+        return console.log(`Le client ${name} ${lastName} a été ajouté avec l'id ${newClient.id}`);
+    } else {
+       return console.log("Le nom et le prénom doivent être des chaînes de caractères");
+    }
+}
+
