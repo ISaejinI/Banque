5cbe88951f93ef7bbfd7fb605329e4fd55cf84c2
Lou-Anne <saejinmx@gmail.com>
2025-11-06 11:14:18 +0100

    Forms de créations et de dépot fonctionnels

--- index.js
+++ index.js
@@ -64,9 +64,11 @@ function createClient(name, lastName) {
             lastName
         }
         datas.clients.push(newClient);
-        return console.info(`Le client ${name} ${lastName} a été ajouté avec l'id ${newClient.id}`);
+        return { status: 'success', message: `Le client ${name} ${lastName} a été ajouté avec l'id ${newClient.id}` };
+        // return console.info(`Le client ${name} ${lastName} a été ajouté avec l'id ${newClient.id}`);
     } else {
-       return console.error("Le nom et le prénom doivent être des chaînes de caractères");
+        //    return console.error("Le nom et le prénom doivent être des chaînes de caractères");
+        return { status: 'error', message: "Le nom et le prénom doivent être des chaînes de caractères" };
     }
 }
 
@@ -74,11 +76,13 @@ function createAccount(idClient, accountName, depositAmount) {
     const client = getClient(idClient);
     const account = getAccount(idClient, accountName);
     const number = isValidNumber(depositAmount);
+    console.log(typeof depositAmount);
     if (client === false || number === false) {
-        return;
+        return { status: 'error', message: "Client ou montant invalide" };
     }
     if (account != false) {
-        return console.error("Le compte existe déjà pour ce client");
+        return { status: 'error', message: "Le compte existe déjà pour ce client" };
+        // return console.error("Le compte existe déjà pour ce client");
     }
     const newAccount = {
         idClient,
@@ -88,7 +92,8 @@ function createAccount(idClient, accountName, depositAmount) {
     }
     createTransaction(newAccount, `Compte créé avec un dépôt initial de ${depositAmount}€`);
     datas.accounts.push(newAccount);
-    return console.info(`Le compte ${accountName} a été créé avec succès avec ${depositAmount}€ à l'intérieur`);
+    // return console.info(`Le compte ${accountName} a été créé avec succès avec ${depositAmount}€ à l'intérieur`);
+    return { status: 'success', message: `Le compte ${accountName} a été créé avec succès avec ${depositAmount}€ à l'intérieur` };
 }
 
 function deleteAccount(idClient, accountName) {
@@ -113,7 +118,8 @@ function deposit(idClient, accountName, depositAmount) {
     }
     account.amount += depositAmount;
     createTransaction(account, `Dépôt de ${depositAmount}€`);
-    return console.info(`Le dépôt de ${depositAmount}€ a été effectué avec succès. Nouveau solde : ${account.amount}€`);
+    // return console.info(`Le dépôt de ${depositAmount}€ a été effectué avec succès. Nouveau solde : ${account.amount}€`);
+    return { status: 'success', message: `Le dépôt de ${depositAmount}€ a été effectué avec succès. Nouveau solde : ${account.amount}€` };
 }
 
 function withdrawal(idClient, accountName, withdrawalAmount) {
@@ -159,7 +165,7 @@ function displayAccountBalance(idClient, accountName) {
     return console.info(`Le solde du compte ${accountName} est de ${account.amount}€`);
 }
 
-function displayTransactions (idClient, accountName) {
+function displayTransactions(idClient, accountName) {
     const client = getClient(idClient);
     const account = getAccount(idClient, accountName);
     if (client === false || account === false) {
@@ -194,7 +200,7 @@ function applyInterests() {
     const interest = 0.015;
     datas.accounts.forEach(account => {
         const lastInterestTransaction = account.transactions.findLast(transaction => transaction.detail == 'Application des intérêts');
-        const copyDateLastInterestTransaction = (lastInterestTransaction === undefined? "" : new Date(lastInterestTransaction.date));
+        const copyDateLastInterestTransaction = (lastInterestTransaction === undefined ? "" : new Date(lastInterestTransaction.date));
         if (account.amount > 0 && lastInterestTransaction === undefined || account.amount > 0 && lastInterestTransaction !== undefined && getCurrentDateTime() >= copyDateLastInterestTransaction.setFullYear(copyDateLastInterestTransaction.getFullYear() + 1)) {
             account.amount += account.amount * interest;
             createTransaction(account, `Application des intérêts`);
@@ -207,7 +213,7 @@ function applyFees() {
     const fee = 2;
     datas.accounts.forEach(account => {
         const lastFeeTransaction = account.transactions.findLast(transaction => transaction.detail == 'Application des frais de tenue de compte');
-        const copyDateLastFeeTransaction = (lastFeeTransaction === undefined? "" : new Date(lastFeeTransaction.date));
+        const copyDateLastFeeTransaction = (lastFeeTransaction === undefined ? "" : new Date(lastFeeTransaction.date));
         if (account.amount > 0 && lastFeeTransaction === undefined || account.amount > 0 && lastFeeTransaction !== undefined && getCurrentDateTime() >= copyDateLastFeeTransaction.setMonth(copyDateLastFeeTransaction.getMonth() + 1)) {
             account.amount -= fee;
             createTransaction(account, `Application des frais de tenue de compte`);
@@ -215,3 +221,92 @@ function applyFees() {
     })
     return console.log("Les frais de tenue de compte ont été appliqués à tous les comptes");
 }
+
+//Fonctionnalités de la V2
+
+//Gestion de la boite d'alerte
+function showAlert(type, message) {
+    const alert = document.getElementById('alert');
+    alert.className = type;
+    alert.innerText = message;
+    alert.style.display = 'block';
+    setTimeout(() => {
+        alert.style.display = 'none';
+    }, 3000);
+}
+
+//Gestion des valeurs des selects
+function putClientsInSelects() {
+    const selects = document.querySelectorAll('.clientIdSelect');
+    const clients = datas.clients;
+    let inputs = `<option selected value="">Sélectionner un client</option>`;
+    clients.forEach(client => {
+        inputs += `<option value="${client.id}">${client.name} ${client.lastName} - ${client.id}</option>`;
+    });
+    selects.forEach(select => {
+        select.innerHTML = '';
+        select.innerHTML += inputs;
+    })
+}
+
+function putAccountsInSelects(idClient, selectId) {
+    const select = document.getElementById(selectId);
+    const accounts = datas.accounts.filter(account => account.idClient === idClient);
+    let inputs = `<option selected value="">Sélectionner un compte</option>`;
+    accounts.forEach(account => {
+        inputs += `<option value="${account.accountName}">${account.accountName}</option>`;
+    });
+    select.innerHTML = '';
+    select.innerHTML += inputs;
+}
+
+// Gestion des formulaires
+document.addEventListener('DOMContentLoaded', () => {
+    putClientsInSelects();
+
+    //Gestion du formulaire de création de client
+    const createClientButton = document.getElementById('createClient');
+    createClientButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        const firstName = document.getElementById('createClientFirstName').value;
+        const lastName = document.getElementById('createClientLastName').value;
+        const result = createClient(firstName, lastName);
+        document.getElementById('createClientFirstName').value = '';
+        document.getElementById('createClientLastName').value = '';
+        showAlert(result.status, result.message);
+        putClientsInSelects();
+    })
+
+    //Gestion du formulaire de création de compte
+    const createAccountButton = document.getElementById('createAccount');
+    createAccountButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        const idClient = document.getElementById('createAccountClient').value;
+        const accountName = document.getElementById('createAccountName').value;
+        const depositAmount = Number(document.getElementById('createAccountInitialDeposit').value);
+        const result = createAccount(idClient, accountName, depositAmount);
+        document.getElementById('createAccountClient').value = '';
+        document.getElementById('createAccountName').value = '';
+        document.getElementById('createAccountInitialDeposit').value = '';
+        showAlert(result.status, result.message);
+    })
+
+    //Gestion du formulaire de dépot d'argent
+    const depositButton = document.getElementById('deposit');
+    const depositAccountSelect = document.getElementById('depositAccountClient');
+    depositAccountSelect.addEventListener('change', (e) => {
+        const idClient = e.target.value;
+        putAccountsInSelects(idClient, 'depositAccountName');
+    });
+    depositButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        const idClient = document.getElementById('depositAccountClient').value;
+        const accountName = document.getElementById('depositAccountName').value;
+        const depositAmount = Number(document.getElementById('depositAmount').value);
+        const result = deposit(idClient, accountName, depositAmount);
+        document.getElementById('depositAccountClient').value = '';
+        document.getElementById('depositAccountName').value = '';
+        document.getElementById('depositAmount').value = '';
+        showAlert(result.status, result.message);
+    })
+});
